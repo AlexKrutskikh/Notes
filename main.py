f52d1a9a2ca193bedde0d658719291955d286126
Alex Krutskikh <krutskih.ay@gmail.com>
2025-02-16 22:30:04 +0300

    add_swager

--- main.py
+++ main.py
@@ -3,10 +3,29 @@ from fastapi import FastAPI
 from auth.endpoints import router as auth_router
 from notes.endpoints import router as notes_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Notes API",
+    description="""
+## 📌 Описание  
+Этот проект представляет собой API для управления заметками с поддержкой ролей пользователей (`User` и `Admin`).  
+**Стек технологий:** FastAPI + MongoDB + JWT + Docker  
 
-app.include_router(auth_router, tags=["auth"])
-app.include_router(notes_router, tags=["notes"])
+### 🚀 Основные функции:  
+- 📌 **Регистрация** по email с кодом подтверждения  
+- 🔑 **Авторизация** через JWT (токен хранится в куках)  
+- 📝 **CRUD-операции** с заметками  
+- 🛡️ **Разграничение доступа** (права `User` и `Admin`)  
+- 📜 **Логирование** действий пользователей  
+- 🐳 **Готовый Dockerfile** и `docker-compose.yml`  
+- ⚙️ **Pytest** для тестирования эндпоинтов  
+""",
+    version="1.0.0"
+)
+
+
+app.include_router(auth_router, prefix="/auth", tags=["Auth🔑"])
+app.include_router(notes_router, prefix="/notes", tags=["Notes📝"])
+
 
 
 if __name__ == "__main__":
